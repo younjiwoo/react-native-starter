a4b5cf9d491a75514f8b8f5b38794f7099e2a3d5
Younji <younjiwoo@gmail.com>
2022-02-20 10:06:43 +0900

    link buttons to different paths

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,19 +3,31 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
 	const friends = [
-		{ name: 'friend 1' },
-		{ name: 'friend 2' },
-		{ name: 'friend 3' },
-		{ name: 'friend 4' },
+		{ name: 'friend 1', age: 11 },
+		{ name: 'friend 2', age: 99 },
+		{ name: 'friend 3', age: 33 },
+		{ name: 'friend 4', age: 33 },
+		{ name: 'friend 5', age: 33 },
+		{ name: 'friend 6', age: 33 },
 	];
 	return (
 		<FlatList
+			keyExtractor={(friend) => friend.name}
 			data={friends}
 			renderItem={({ item }) => {
-				return <Text key={item.index}>{item.name}</Text>;
+				return (
+					<Text style={styles.textStyle} key={item.index}>
+						{item.name} - Age {item.age}
+					</Text>
+				);
 			}}
 		/>
 	);
 };
 
+const styles = StyleSheet.create({
+	textStyle: {
+		marginVertical: 50,
+	},
+});
 export default ListScreen;
